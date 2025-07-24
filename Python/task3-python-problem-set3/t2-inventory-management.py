7ac5d1d855e56f41652337fee8ac5d2455758294
dkolekar-cci <deepa.kolekar@creativecapsule.com>
2025-07-24 06:36:53 +0530

    Updated t2-inventory-management.py

--- Python/task3-python-problem-set3/t2-inventory-management.py
+++ Python/task3-python-problem-set3/t2-inventory-management.py
@@ -1,42 +1,70 @@
 class Product:
-    def __init__(self,product_id=0,name=0,quantity=0,price=0):
+    def __init__(self, product_id, name, quantity, price):
         self.__product_id = product_id
         self.__name = name
         self.__quantity = quantity
         self.__price = price
 
-    def get_product_id(self):
+    @property
+    def product_id(self):
         return self.__product_id
-    
-    def get_name(self):
+
+    @product_id.setter
+    def product_id(self, new_id):
+        self.__product_id = new_id
+
+    @property
+    def name(self):
         return self.__name
-    
-    def get_quantity(self):
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    @property
+    def quantity(self):
         return self.__quantity
-    
-    def get_price(self):
-        return self.__price 
-    
-    def set_product_id(self, product_id):
-        self.__product_id= product_id
 
-    def set_name(self, name):
-        self.__name= name
+    @quantity.setter
+    def quantity(self, new_quantity):
+        self.__quantity = new_quantity
 
-    def set_quantity(self, quantity):
-        self.__quantity = quantity
+    @property
+    def price(self):
+        return self.__price
 
-    def set_price(self, price ):
-        self.__price = price
-  
+    @price.setter
+    def price(self, new_price):
+        self.__price = new_price
+
+
     def get_value(self):
-        self.__quantity * self.__price  
+        return self.__quantity * self.__price
 
-      
+class PerishableProduct(Product):
+    def __init__(self, product_id, name, quantity, price, expiring_soon):
+        super().__init__(product_id, name, quantity, price)
+        self.expiring_soon = expiring_soon
 
+    def get_value(self):
+        if self.expiring_soon:  
+            return super().get_value() * 0.3
+        else:
+            return super().get_value()
 
+Product_1 = Product("52A13", "Rice", 50, 400)
+Product_2 = Product("52A14", "Sugarr", 30, 200)
+Product_3 = PerishableProduct("53B11", "Bread", 20, 150, True)
+Product_4 = PerishableProduct("53B12", "Cheese", 10, 300, False)
 
+product_list = [Product_1, Product_2, Product_3, Product_4]
 
-    
-    
-        
+total_inventory_value = 0
+
+print("LIST OF PRODUCTS:")
+for item in product_list:
+    value = item.get_value()
+    print(f"{item.name} - Rs {value}")
+    total_inventory_value += value
+
+print("Total inventory value is: Rs", total_inventory_value)
