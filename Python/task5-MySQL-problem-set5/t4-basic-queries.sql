67467455d0a39dc6851016f7bf73c05cd987c5fb
dkolekar-cci <deepa.kolekar@creativecapsule.com>
2025-07-28 09:08:29 +0530

    Create t4-basic-queries.sql

---
+++ Python/task5-MySQL-problem-set5/t4-basic-queries.sql
@@ -0,0 +1,11 @@
+SELECT * FROM users;
+
+SELECT * FROM users WHERE department = 'Computer Science';
+
+SELECT * FROM users WHERE age > 20;
+
+SELECT * FROM users WHERE age <= 20;
+
+SELECT * FROM users WHERE age != 23;
+
+SELECT * FROM users WHERE age <=20 AND age>= 21;
